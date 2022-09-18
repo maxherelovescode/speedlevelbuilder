26bb70d3eb1e8de6c1cc9fc86bf5eefdf7a71ac8
Max H <maxplaysbest@gmail.com>
2022-09-18 14:58:00 +1200

    Started login screen

---
+++ SpeedLevelBuilder/Startup.Designer.cs
@@ -0,0 +1,93 @@
+﻿using System.ComponentModel;
+
+namespace SpeedLevelBuilder
+{
+    partial class Startup
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Startup));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.logo = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.logo)).BeginInit();
+            this.SuspendLayout();
+            //
+            // button1
+            //
+            this.button1.Location = new System.Drawing.Point(167, 342);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(143, 65);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Login";
+            this.button1.UseVisualStyleBackColor = true;
+            //
+            // button2
+            //
+            this.button2.Location = new System.Drawing.Point(473, 342);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(143, 65);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Sign-Up";
+            this.button2.UseVisualStyleBackColor = true;
+            //
+            // logo
+            //
+            this.logo.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.logo.Image = ((System.Drawing.Image)(resources.GetObject("logo.Image")));
+            this.logo.InitialImage = ((System.Drawing.Image)(resources.GetObject("logo.InitialImage")));
+            this.logo.Location = new System.Drawing.Point(57, -48);
+            this.logo.Name = "logo";
+            this.logo.Size = new System.Drawing.Size(668, 559);
+            this.logo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.logo.TabIndex = 2;
+            this.logo.TabStop = false;
+            //
+            // Startup
+            //
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(48)))), ((int)(((byte)(18)))), ((int)(((byte)(0)))));
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.logo);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Startup";
+            this.Text = "Startup";
+            ((System.ComponentModel.ISupportInitialize)(this.logo)).EndInit();
+            this.ResumeLayout(false);
+        }
+
+        private System.Windows.Forms.PictureBox logo;
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+
+        #endregion
+    }
+}
---
+++ SpeedLevelBuilder/Startup.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace SpeedLevelBuilder
+{
+    public partial class Startup : Form
+    {
+        public Startup()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
